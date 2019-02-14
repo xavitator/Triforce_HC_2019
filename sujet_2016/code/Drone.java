49423c488ee24bfab3480e5b0dcbd88fedb0ba03
fauconfan <joseph0priou@gmail.com>
2019-02-14 10:01:45 +0100

    add default main

--- sujet_2016/code/Drone.java
+++ sujet_2016/code/Drone.java
@@ -7,12 +7,14 @@
 class Drone {
 	Point		pos;
 	int			[]products;
+	int			timeout;
 	// DroneType	type;
 
 	Drone(Point pos, int []products /*, DroneType type */)
 	{
 		this.pos = pos;
 		this.products = products;
+		this.timeout = -1;
 		// this.type = type;
 	}
 }
--- sujet_2016/code/Main.java
+++ sujet_2016/code/Main.java
@@ -1,5 +1,14 @@
 
 class Main {
+	static int row = 0;
+	static int col = 0;
+	static int Deadline = 0;
+	static int DroneCapacity = 0;
+	static int []Products;
+	static Warehouse []Warehouses;
+	static Order []Orders;
+	static Drone []Drones;
+
 	public static void main(String[] args) {
 		System.out.println("Hello");
 	}
