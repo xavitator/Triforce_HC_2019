f417e43ddffa7e363f1afff571e5a9e45ed0bff5
Étienne Marais <etiennemarais@protonmail.com>
2019-02-14 10:55:31 +0100

    Update main

--- sujet_2016/code/Main.java
+++ sujet_2016/code/Main.java
@@ -10,13 +10,18 @@ class Main {
 	static Drone []drones;
 
 	public static void main(String[] args) {
-		ReadFile.setup("../busy_day.in");
+		ReadFile.setup("../test.in");
 		String [] intel  = ReadFile.getintel();
 		row = Integer.parseInt(intel[0]);
 		col = Integer.parseInt(intel[1]);
+		System.out.println("Row : " + row);
+		System.out.println("Column : " + col);
 		deadline = Integer.parseInt(intel[3]);
+		System.out.println("Deadline : " + deadline);
 		drones = new Drone[Integer.parseInt(intel[2])];
+		System.out.println("Drone 0 : " + drones[0]);
 		droneCapacity = Integer.parseInt(intel[4]);
+		System.out.println("Capacity : " + droneCapacity);
 		int nbProducts = ReadFile.getNbProduct();
 		products = ReadFile.getProductWeight();
 		warehouses = ReadFile.getWarehousesIntel(nbProducts);
