b928512faac11906d4f12ed0d4481852849c3816
Kolibs <etienne@marais.green>
2019-02-18 20:07:10 +0100

    Ajout commentaire

--- sujet_2016/code/ReadFile.java
+++ sujet_2016/code/ReadFile.java
@@ -14,7 +14,7 @@ public class ReadFile {
         try {
             lignes = (new BufferedReader(new InputStreamReader(
                     new FileInputStream(src)
-            ))).lines().iterator();
+            ))).lines().iterator(); // Itérateur sur les lignes
         } catch(Exception e) {
             e.printStackTrace();
             System.exit(0);
@@ -22,15 +22,15 @@ public class ReadFile {
     }
 
     static String[] getintel() {
-        String[] mapintel = lignes.next().split(" ");
+        String[] mapintel = lignes.next().split(" "); // Récupère la première ligne
         return mapintel;
     }
 
-    static int getNbProduct() {
+    static int getNbProduct() { // Récupère le nombre de produits globaux
         return Integer.parseInt(lignes.next());
     }
 
-    static int[] getProductWeight() {
+    static int[] getProductWeight() { // Récupère le poids de chaque produit
         String[] products = lignes.next().split(" ");
         int[] weight = new int[products.length];
         for (int i = 0 ; i < products.length ; i++) {
@@ -39,23 +39,23 @@ public class ReadFile {
         return weight;
     }
 
-    static int getNbWarehouses() {
+    static int getNbWarehouses() { // Donne le nombre total d'entrepots
         return Integer.parseInt(lignes.next());
     }
 
-    static Warehouse getWarehouseIntel(int nbProduct) {
+    static Warehouse getWarehouseIntel(int nbProduct) { // Rempli les données pour un entrepot
         Warehouse w = new Warehouse();
         String[] line = lignes.next().split(" ");
-        w.pos = new Point(Integer.parseInt(line[0]), Integer.parseInt(line[1]));
+        w.pos = new Point(Integer.parseInt(line[0]), Integer.parseInt(line[1])); // Position
         line = lignes.next().split(" ");
         w.products = new int[nbProduct];
         for(int i = 0 ; i  < line.length ; i++) {
-            w.products[i] = Integer.parseInt(line[i]);
+            w.products[i] = Integer.parseInt(line[i]); // Indice produit et le nombre
         }
         return w;
     }
 
-    static Warehouse[] getWarehousesIntel(int nbProduct) {
+    static Warehouse[] getWarehousesIntel(int nbProduct) { // Ensemble des données des entrepots
         int n = getNbWarehouses();
         Warehouse [] intels = new Warehouse[n];
         for(int i = 0 ; i < n ; i++) {
@@ -64,11 +64,11 @@ public class ReadFile {
         return intels;
     }
 
-    static int nbCustomers() {
+    static int nbCustomers() { // Nombre de clients globaux
         return Integer.parseInt(lignes.next());
     }
 
-    static Order CustomerOrder() {
+    static Order CustomerOrder() { // Récupère les commandes d'un client
         String[] values = lignes.next().split(" ");
         Order o = new Order();
         o.pos = new Point(Integer.parseInt(values[0]),Integer.parseInt(values[1]));
@@ -81,7 +81,7 @@ public class ReadFile {
         return o;
     }
 
-    static  Order[] CustomersOrders() {
+    static  Order[] CustomersOrders() { // Récupère l'ensemble des données de commandes des clients
         int n = nbCustomers();
         Order [] customers = new Order[n];
         for (int i = 0 ; i < n ; i++) {
