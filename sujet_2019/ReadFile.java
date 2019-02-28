67f7c5a7000ccdfa2756a1e4379924c4f4b39615
Kolibs <etienne@marais.green>
2019-02-28 21:26:34 +0100

    With states

--- sujet_2019/ReadFile.java
+++ sujet_2019/ReadFile.java
@@ -4,7 +4,9 @@ import java.util.*;
 
 public class ReadFile {
     static Iterator<String> lignes;
-
+    static int mid_size;
+    static int vert;
+    static int hori;
     /**
      * Constructeur
      * @param src fichier à lire
@@ -23,6 +25,7 @@ public class ReadFile {
 
     static LinkedList<String> getTags(String[] lines) {
         int nbTags = Integer.parseInt(lines[1]);
+        mid_size += nbTags;
         LinkedList<String> tags = new LinkedList<>();
         for (int i = 2 ; i < nbTags +2 ; i++) {
             tags.add(lines[i]);
@@ -37,9 +40,18 @@ public class ReadFile {
             String [] lines = lignes.next().split(" ");
             res[i] = new Photo(i);
             res[i].vertical = lines[0].equals("V");
+            if (res[i].vertical)
+                vert++;
+            else
+                hori++;
             res[i].tags = getTags(lines);
             //System.out.print("Debug : " + res[i] + "\n");
         }
+        mid_size /= nb;
+        System.out.println("Nombre de photos : " + nb);
+        System.out.println("Horizontal : " + hori);
+        System.out.println("Vertical : " + vert);
+        System.out.println("Nombre moyen de tags : " + mid_size);
         return res;
     }
 
