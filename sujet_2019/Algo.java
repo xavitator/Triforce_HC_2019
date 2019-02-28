0c69b39757378c83a1d145bec9478b813a3859e5
Xavitator <xavier75013@gmail.com>
2019-02-28 20:09:46 +0100

    ajout ajout stupid

---
+++ sujet_2019/Algo.java
@@ -0,0 +1,92 @@
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Algo {
+
+
+    static LinkedList<Photo>[] separer(Photo[] content){
+        LinkedList<Photo>[] res = new LinkedList[2];
+        res[0] = new LinkedList<>();
+        res[1] = new LinkedList<>();
+        for (int i = 0; i < content.length; i++) {
+            if(content[i].vertical){
+                res[1].addLast(content[i]);
+            }
+            else res[0].addLast(content[i]);
+        }
+        return res;
+    }
+
+    static boolean haveOneCommun(Picture p1, Picture p2, Picture comp){
+        boolean wp1 = false;
+        boolean wp2 = false;
+        for (String el : comp.tags){
+            if(p1.tags.contains(el)) wp1 = true;
+            if(p2.tags.contains(el)) wp2 = true;
+            if(wp1 && wp2) return true;
+        }
+        return false;
+    }
+
+    static boolean communOne(Picture p, Picture comp){
+        for (String el : comp.tags){
+            if(p.tags.contains(el)) return true;
+        }
+        return false;
+    }
+
+    static boolean isAdded (LinkedList<Picture> pict, Picture el){
+        if (pict.isEmpty()){
+            pict.addLast(el);
+            return true;
+        }
+        for (int i = 0; i < pict.size() - 1; i++) {
+            if(haveOneCommun(pict.get(i), pict.get(i+1), el)){
+                pict.add(i+1, el);
+                return true;
+            }
+        }
+        if(communOne(pict.getFirst(), el)){
+            pict.addFirst(el);
+            return true;
+        }
+        if(communOne(pict.getLast(), el)){
+            pict.addLast(el);
+            return true;
+        }
+        return false;
+    }
+
+    static LinkedList<Picture> startAlgo(LinkedList<Picture> pict){
+        boolean all = false;
+        LinkedList<Picture> res = new LinkedList<>();
+        LinkedList<Picture> tmp = new LinkedList<>();
+        while(all == false){
+            all =true;
+            for (Picture el : pict){
+                if(! isAdded(res, el)){
+                    tmp.addLast(el);
+                }
+                else all = false;
+            }
+            pict = tmp;
+            tmp = new LinkedList<>();
+        }
+        return res;
+    }
+
+     static LinkedList<Picture> startStupid(Photo[] content) {
+        LinkedList<Photo>[] separeted = separer(content);
+        LinkedList<Picture> pict = new LinkedList<>();
+        for (Photo el : separeted[0]){
+            pict.addLast(new Picture(el));
+        }
+        for (int i = 0; i < separeted[1].size() - 1; i+=2) {
+            pict.addLast(new Picture(content[i], content[i+1]));
+        }
+        Collections.sort(pict, (Picture a, Picture b) -> b.tags.size() - a.tags.size());
+        return startAlgo(pict);
+
+    }
+
+}
--- sujet_2019/Main.java
+++ sujet_2019/Main.java
@@ -1,11 +1,14 @@
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("====  PARSER ===");
         ReadFile.setup(args[0]);
-        ReadFile.getContent(); // return Photo[]
+        Photo[] content = ReadFile.getContent(); // return Photo[]
 
+        LinkedList<Picture> res = Algo.startStupid(content);
         System.out.println("====  OUTPUT ===");
-        Output.writeResult(args[1], null); // Write and print
+        Output.writeResult(args[1], res); // Write and print
     }
 }
